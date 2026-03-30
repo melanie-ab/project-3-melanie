9fe568459728835c7a8d41862390bb207b665daf
Melanie <mabarca1@luc.edu>
2026-03-30 01:49:17 -0500

    Add jline REPL and multiline input

--- build.sbt
+++ build.sbt
@@ -5,7 +5,9 @@ version := "0.4"
 libraryDependencies ++= Seq(
   "org.scala-lang.modules" %% "scala-parser-combinators" % "2.4.0",
   "org.json4s"             %% "json4s-native"            % "4.1.0-M5",
-  "org.scalatest"          %% "scalatest"                % "3.2.19" % Test
+  "org.scalatest"          %% "scalatest"                % "3.2.19" % Test,
+  "org.jline"              %  "jline"                    % "4.0.9"
+
 )
 
 enablePlugins(JavaAppPackaging)
--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -26,13 +26,40 @@ object Calculator:
       println(unparse(program))
 
   def main(args: Array[String]): Unit =
+    import org.jline.reader.{EndOfFileException, LineReaderBuilder, UserInterruptException}
+    import org.jline.terminal.TerminalBuilder
+
     if args.length > 0 then
       processInput(args.mkString(" "))
     else
-      print("minijs> ")
-      scala.io.Source.stdin.getLines().foreach:
-        line =>
-          processInput(line)
-          print("minijs> ")
+      val terminal = TerminalBuilder.builder().system(true).build()
+      val reader = LineReaderBuilder.builder().terminal(terminal).build()
+
+      try
+        while true do
+          val inputBuilder = new StringBuilder
+          var reading = true
+
+          while reading do
+            val prompt =
+              if inputBuilder.isEmpty then "minijs> "
+              else "... "
+
+            val line = reader.readLine(prompt)
+
+            if line.trim.isEmpty then
+              reading = false
+            else
+              inputBuilder.append(line).append("\n"): Unit
+
+          val input = inputBuilder.toString.trim
+          if input.nonEmpty then
+            processInput(input)
+
+      catch
+        case _: UserInterruptException =>
+          println("\nExiting...")
+        case _: EndOfFileException =>
+          println("\nGoodbye!")
 
 end Calculator
