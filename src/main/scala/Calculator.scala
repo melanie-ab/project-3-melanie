fec5456d9fe2c48540347f2cc03614d952f05920
melanie-ab <mabarca1@luc.edu>
2026-04-17 11:12:56 -0500

    Implement processInput method in Calculator
    Added processInput method to handle user input and display parsed statements.

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -2,6 +2,34 @@ package edu.luc.cs.laufer.cs371.expressions
 
 object Calculator:
 
+  def processInput(input: String): Unit =
+    println("Memory: " + behaviors.memory)
+    println("You entered: " + input)
+
+    val result = ASTBuilder.parseAll(ASTBuilder.repl, input)
+
+    if result.isEmpty then
+      println("This input could not be parsed")
+    else
+      import org.json4s.native.JsonMethods.{pretty, render}
+      import behaviors.*
+
+      val program = result.get
+
+      println("The parsed statements are:")
+      program.foreach(println)
+
+      println("The corresponding JSON structure is:")
+      if program.length == 1 then
+        println(pretty(render(toJson(program.head))))
+      else
+        println(pretty(render(toJson(Block(program)))))
+
+      println("The unparsed statements are:")
+      println(unparse(program))
+
+      println("It evaluates to " + evaluate(program))
+      println("Memory: " + memory)
 
 
   def main(args: Array[String]): Unit =
